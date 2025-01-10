5c31bf11ff65395fa935f3194c0c1f0885ad80c6
Areti Makropoulos <makropoulos.ar@northeastern.edu>
2025-01-09 17:38:47 -0800

    Vector3D class

---
+++ Vector3D.java
@@ -0,0 +1,15 @@
+public class Vector3D {
+    final double x;
+    final double y;
+    final double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

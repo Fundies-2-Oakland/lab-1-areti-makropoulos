dbc58bcd70b9bfd7aeee8f82879bd31d6100c3ad
Areti Makropoulos <makropoulos.ar@northeastern.edu>
2025-01-09 18:04:58 -0800

    problem 1 and 2

--- Vector3D.java
+++ Vector3D.java
@@ -1,12 +1,24 @@
 public class Vector3D {
-    final double x;
-    final double y;
-    final double z;
+    double x;
+    double y;
+    double z;
 
     public Vector3D(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    double getZ() {
+        return z;
     }
 
     public static void main(String[] args) {
